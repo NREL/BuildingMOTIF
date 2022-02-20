e8befdb31e4329536d048b74d400b87c16ff10ea
Matt Steen <matthew.steen@nrel.gov>
2022-02-19 17:06:51 -0700

    don't import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..')) # /../buildingmotif'))
-import buildingmotif
+sys.path.insert(0, os.path.abspath('../../buildingmotif'))
 
 
 # -- Project information -----------------------------------------------------
@@ -48,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
