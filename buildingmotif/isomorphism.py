00089e703135b43516be7c5b26a97d0029d02b10
Gabe Fierro <gtfierro@mines.edu>
2022-03-08 09:28:59 -0700

    add isomorphism implementation

---
+++ buildingmotif/isomorphism.py
@@ -0,0 +1,110 @@
+"""
+Using the VF2 algorithm to compute subgraph isomorphisms between a template T and a graph G.
+If the found isomorphism is a subgraph of T, then T is not fully matched and additional
+input is required to fully populate the template.
+"""
+from collections.abc import Callable
+from itertools import combinations
+from typing import Dict, Generator, Tuple
+
+import networkx as nx
+from networkx.algorithms.isomorphism import DiGraphMatcher
+from rdflib import Graph
+from rdflib.extras.external_graph_libs import rdflib_to_networkx_digraph
+from rdflib.term import Node
+
+from buildingmotif.namespaces import OWL, RDF, RDFS
+
+
+def get_semantic_feasibility(ontology: Graph) -> Callable[[Node, Node], bool]:
+    """
+    Returns a function that checks if two nodes are semantically feasible to be
+    matched given the information in the provided ontology.
+
+    The function returns true if the two nodes are semantically feasible to be matched.
+    We use the following checks:
+    1. if the two nodes are both classes, then one must be a subclass of the ot her
+    2. if the two nodes are instances, then they must be of the s
+    TODO: other checks?
+    """
+
+    def semantic_feasibility(g1: Node, g2: Node) -> bool:
+        # case 0: same node
+        if g1 == g2:
+            return True
+        # case 1: both are classes
+        if (g1, RDF.type, OWL.Class) in ontology and (
+            g2,
+            RDF.type,
+            OWL.Class,
+        ) in ontology:
+            if g2 in ontology.transitive_objects(g1, RDFS.subClassOf):
+                return True
+            elif g1 in ontology.transitive_objects(g2, RDFS.subClassOf):
+                return True
+            else:
+                return False
+        return True
+
+    return semantic_feasibility
+
+
+class VF2SemanticMatcher(DiGraphMatcher):
+    """
+    A subclass of DiGraphMatcher that incorporates semantic feasibility into the matching
+    process using the provided ontology.
+
+    :param T: The template graph
+    :param G: The building graph
+    :param ontology: The ontology that contains the information about node semantics
+    """
+
+    def __init__(self, T: Graph, G: Graph, ontology: Graph):
+        super().__init__(T, G)
+        self.ontology = ontology
+        self._semantic_feasibility = get_semantic_feasibility(ontology)
+
+    def semantic_feasibility(self, g1: Node, g2: Node) -> bool:
+        """
+        Returns true if the two nodes are semantically feasible to be matched
+        """
+        return self._semantic_feasibility(g1, g2)
+
+
+def generate_all_template_subgraphs(T: Graph) -> Generator[nx.DiGraph, None, None]:
+    """
+    Generates all node-induced subgraphs of T in order of increasing size
+    """
+    for nodecount in range(len(T.all_nodes())):
+        for nodelist in combinations(T.all_nodes(), nodecount):
+            yield rdflib_to_networkx_digraph(T).subgraph(nodelist)
+
+
+def digraph_to_rdflib(digraph: nx.DiGraph) -> Graph:
+    """
+    Turns a nx.DiGraph into an rdflib.Graph. Expects the nx.DiGraph to have been
+    produced by rdflib_to_networkx_digraph.
+    """
+    g = Graph()
+    for s, o, pdict in digraph.edges(data=True):
+        g.add((s, pdict["triples"][0][1], o))
+    return g
+
+
+def find_largest_subgraph_monomorphism(
+    T: Graph, G: Graph, ontology: Graph
+) -> Tuple[Dict[Node, Node], Graph]:
+    """
+    Returns the largest subgraph of T that is monomorphic to G.
+    """
+    B = rdflib_to_networkx_digraph(G)
+    largest_mapping = {}
+    largest_subgraph = nx.DiGraph()
+    for Tsubgraph in generate_all_template_subgraphs(T):
+        matching = VF2SemanticMatcher(B, Tsubgraph, ontology)
+        if matching.subgraph_is_monomorphic():
+            for sg in matching.subgraph_monomorphisms_iter():
+                if len(sg) > len(largest_mapping):
+                    largest_mapping = sg
+                    largest_subgraph = Tsubgraph
+    return largest_mapping, digraph_to_rdflib(largest_subgraph)
