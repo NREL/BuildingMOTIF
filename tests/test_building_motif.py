e6b2a924ee1ade362c09a90b0d37d5354eabb41c
Gabe Fierro <gtfierro@mines.edu>
2022-05-31 09:07:01 -0600

    make brick ontology test quicker

--- tests/test_building_motif.py
+++ tests/test_building_motif.py
@@ -5,7 +5,7 @@ def test_load_library_from_ontology():
     bm = BuildingMotif("sqlite:///")
     lib = bm.load_library(ontology_graph="tests/fixtures/Brick1.3rc1-equip-only.ttl")
     assert lib is not None
-    assert len(lib.get_templates()) == 344
+    assert len(lib.get_templates()) == 2
     # spot check a certain template
     templ = lib.get_template_by_name("https://brickschema.org/schema/Brick#AHU")
     assert templ is not None
@@ -15,7 +15,7 @@ def test_load_library_from_ontology():
 
 
 def test_load_library_from_directory():
-    bm = BuildingMotif("sqlite:///")
+    bm = BuildingMotif("sqlite://")
     lib = bm.load_library(directory="tests/fixtures/templates")
     assert lib is not None
     assert len(lib.get_templates()) == 6
