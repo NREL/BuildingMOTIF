53a098e688276b1b2040eee3e9b0a1940e78ae9f
TShapinsky <tobiasshapinsky@gmail.com>
2022-06-03 11:10:34 -0600

    wrap sqlalchemy engine to allow sessioned usage with stock rdflib sqlalchemy

--- buildingmotif/building_motif/building_motif.py
+++ buildingmotif/building_motif/building_motif.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -24,7 +26,9 @@ class BuildingMOTIF(metaclass=Singleton):
         self.session = Session()
 
         self.table_connection = TableConnection(self.engine, self)
-        self.graph_connection = GraphConnection(self.engine, self)
+        self.graph_connection = GraphConnection(
+            BuildingMotifEngine(self.engine, self.session)
+        )
 
     def close(self) -> None:
         """Close session and engine."""
@@ -39,3 +43,22 @@ def get_building_motif() -> "BuildingMOTIF":
     if hasattr(BuildingMOTIF, "instance"):
         return BuildingMOTIF.instance  # type: ignore
     raise SingletonNotInstantiatedException
+
+
+class BuildingMotifEngine:
+    def __init__(self, engine, session) -> None:
+        self.engine = engine
+        self.session = session
+
+    @contextmanager
+    def begin(self):
+        yield self.session
+
+    @contextmanager
+    def connect(self):
+        yield self.session
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        return getattr(self.engine, attr)
