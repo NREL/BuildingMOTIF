112dfe7c5a2a006359356c360a87e4d10fd931df
Gabe Fierro <gtfierro@mines.edu>
2022-06-29 17:10:35 -0600

    refactor table setup

--- buildingmotif/building_motif/building_motif.py
+++ buildingmotif/building_motif/building_motif.py
@@ -34,26 +34,36 @@ class BuildingMOTIF(metaclass=Singleton):
 
         self.setup_logging(log_level)
 
-        # done with initialization: rest of this code is specific
-        # to detecting in-memory sqlite databases
+        # setup tables automatically if using a in-memory sqlite database
+        if self._is_in_memory_sqlite():
+            self.setup_tables()
+
+        self.table_connection = TableConnection(self.engine, self)
+        self.graph_connection = GraphConnection(
+            BuildingMotifEngine(self.engine, self.session)
+        )
+
+    def setup_tables(self):
+        """
+        Creates the tables in the underlying database
+        """
+        BuildingMOTIFBase.metadata.create_all(self.engine)
+
+    def _is_in_memory_sqlite(self) -> bool:
+        """
+        Returns true if the BuildingMOTIF instance uses an in-memory sqlite database
+        """
         if self.engine.dialect.name != "sqlite":
-            return
-        # detect if this is a sqlite3 in-memory database.
-        # Do this by getting the 'filename' of the database; if this is empty, the db is in-memory
+            return False
+        # get the 'filename' of the database; if this is empty, the db is in-memory
         raw_conn = self.engine.raw_connection()
         filename = (
             raw_conn.cursor()
             .execute("select file from pragma_database_list where name='main';", ())
             .fetchone()
         )
-        in_memory = not len(filename[0])
-        if in_memory:
-            BuildingMOTIFBase.metadata.create_all(self.engine)
-
-        self.table_connection = TableConnection(self.engine, self)
-        self.graph_connection = GraphConnection(
-            BuildingMotifEngine(self.engine, self.session)
-        )
+        # length is 0 if the db is in-memory
+        return not len(filename[0])
 
     def setup_logging(self, log_level):
         """Create log file with DEBUG level and stdout handler with specified log_level"""
