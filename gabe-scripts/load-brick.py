3b2bef875479d3c1e48d6c85814abb0dbe71d638
Gabe Fierro <gtfierro@mines.edu>
2022-07-15 22:59:29 -0600

    remove accidental addition

--- gabe-scripts/load-brick.py
+++
@@ -1,5 +0,0 @@
-from buildingmotif import BuildingMOTIF
-from buildingmotif.dataclasses import Library
-
-bm = BuildingMOTIF("sqlite://")
-Library.load(ontology_graph="libraries/brick/Brick-subset.ttl")
