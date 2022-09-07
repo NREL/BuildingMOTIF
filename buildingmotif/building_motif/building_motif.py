a3a1bb587aab1d042f556c35e0be9d2cdcdb36d2
Gabe Fierro <gtfierro@mines.edu>
2022-09-06 20:12:48 -0600

    custom serde should work for args now

--- buildingmotif/building_motif/building_motif.py
+++ buildingmotif/building_motif/building_motif.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from contextlib import contextmanager
@@ -17,6 +18,37 @@ from buildingmotif.database.tables import Base as BuildingMOTIFBase
 from buildingmotif.namespaces import bind_prefixes
 
 
+# the custom ser/de handlers are to allow the database to ensure
+# uniqueness of dependency bindings (see https://github.com/NREL/BuildingMOTIF/pull/113)
+def _custom_json_serializer(obj):
+    """
+    Serializes dictionaries as a sorted list of key-value tuples. All
+    other items are serialized as normal
+    """
+    if isinstance(obj, dict):
+        # ensure dictionary has at least 1 pair
+        if len(obj) == 0:
+            obj[None] = None
+        return json.dumps(sorted([(k, v) for k, v in obj.items()]))
+    return json.dumps(obj)
+
+
+def _custom_json_deserislizer(inp):
+    """
+    Handles deserializing the objects serialied by _custom_json_serializer
+    """
+    obj = json.loads(inp)
+    # return normal object if it is not a list
+    if not isinstance(obj, list):
+        return obj
+    # if *all* of the items in the list are pairs,
+    # then we deserialize as a dictionary
+    if len(obj) > 0 and all(map(lambda x: isinstance(x, list) and len(x) == 2, obj)):
+        return dict(obj)
+    # ...otherwise return. It's just a normal list!
+    return obj
+
+
 class BuildingMOTIF(metaclass=Singleton):
     """Manages BuildingMOTIF data classes."""
 
@@ -31,7 +63,12 @@ class BuildingMOTIF(metaclass=Singleton):
         :default log_level: INFO
         """
         self.db_uri = db_uri
-        self.engine = create_engine(db_uri, echo=False)
+        self.engine = create_engine(
+            db_uri,
+            echo=False,
+            json_serializer=_custom_json_serializer,
+            json_deserializer=_custom_json_deserislizer,
+        )
         self.session_factory = sessionmaker(bind=self.engine, autoflush=True)
         self.Session = scoped_session(self.session_factory)
 
