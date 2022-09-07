8332e939c718aba848a1998542401aaf553bb476
Hannah Eslinger <heslinge@nrel.gov>
2022-09-07 16:09:28 -0600

    Add Get/Patch model graph

--- buildingmotif/api/views/model.py
+++ buildingmotif/api/views/model.py
@@ -1,7 +1,12 @@
 import flask
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
+from flask_api import status
+from rdflib import Graph
+from rdflib.plugins.parsers.notation3 import BadSyntax
+from sqlalchemy.orm.exc import NoResultFound
 
 from buildingmotif.api.serializers.model import serialize
+from buildingmotif.dataclasses import Model
 
 blueprint = Blueprint("models", __name__)
 
@@ -27,6 +32,60 @@ def get_model(models_id: int) -> flask.Response:
     :return: requested id
     :rtype: Model
     """
-    model = current_app.building_motif.table_connection.get_db_model(models_id)
+    try:
+        model = current_app.building_motif.table_connection.get_db_model(models_id)
+    except NoResultFound:
+        return {"message": f"No model with id {models_id}"}, status.HTTP_404_NOT_FOUND
 
-    return jsonify(serialize(model))
+    return jsonify(serialize(model)), status.HTTP_200_OK
+
+
+@blueprint.route("/<models_id>/graph", methods=(["GET"]))
+def get_model_graph(models_id: int) -> flask.Response:
+    """get model with id
+
+    :param models_id: model id
+    :type models_id: int
+    :return: requested id
+    :rtype: Model
+    """
+    try:
+        model = Model.load(models_id)
+    except NoResultFound:
+        return {"message": f"No model with id {models_id}"}, status.HTTP_404_NOT_FOUND
+
+    return model.graph.serialize(format="ttl"), status.HTTP_200_OK
+
+
+@blueprint.route("/<models_id>/graph", methods=(["PATCH"]))
+def update_model_graph(models_id: int) -> flask.Response:
+    """update model graph
+
+    Takes xml body of ttl formated graph.
+
+    :param models_id: model id
+    :type models_id: int
+    :return: Updated model
+    :rtype: Model
+    """
+    try:
+        model = Model.load(models_id)
+    except NoResultFound:
+        return {"message": f"No model with id {models_id}"}, status.HTTP_404_NOT_FOUND
+
+    if request.content_type != "application/xml":
+        return {
+            "message": "request content type must be xml"
+        }, status.HTTP_400_BAD_REQUEST
+
+    try:
+        graph = Graph().parse(data=request.data, format="ttl")
+    except BadSyntax as e:
+        return {"message": f"data is unreadable: {e}"}, status.HTTP_400_BAD_REQUEST
+
+    model.graph.remove((None, None, None))
+    model.add_graph(graph)
+
+    current_app.building_motif.session.commit()
+
+    return model.graph.serialize(format="ttl")
--- tests/unit/api/test_model.py
+++ tests/unit/api/test_model.py
@@ -1,4 +1,28 @@
+from rdflib import Graph, URIRef
+from rdflib.compare import isomorphic, to_isomorphic
+from rdflib.namespace import RDF
+
 from buildingmotif.dataclasses import Model
+
+graph_data = """
+    @prefix owl: <http://www.w3.org/2002/07/owl#> .
+    @prefix sh: <http://www.w3.org/ns/shacl#> .
+    @prefix : <urn:model#> .
+    @prefix bmotif: <https://nrel.gov/BuildingMOTIF#> .
+
+    :shape1 a bmotif:HVAC ;
+    .
+
+    :shape2 a bmotif:Lighting ;
+    .
+
+    :shape3 a bmotif:Electrical ;
+    .
+"""
+
+default_graph = [
+    (URIRef("my_model"), RDF.type, URIRef("http://www.w3.org/2002/07/owl#Ontology"))
+]
 
 
 def test_get_all_models(client, building_motif):
@@ -39,3 +63,97 @@ def test_get_model(client, building_motif):
         "name": db_model.name,
         "graph_id": db_model.graph_id,
     }
+
+
+def test_get_model_not_found(client):
+    # Act
+    results = client.get("/models/-1")
+
+    # Assert
+    assert results.status_code == 404
+    assert results.json == {"message": "No model with id -1"}
+
+
+def test_get_model_graph(client, building_motif):
+    # Setup
+    model = Model.create(name="urn:my_model")
+    model.add_graph(Graph().parse(data=graph_data, format="ttl"))
+    excepted_graph = to_isomorphic(model.graph)
+    building_motif.session.commit()
+
+    # Act
+    results = client.get(f"/models/{model.id}/graph")
+
+    # Assert
+    assert results.status_code == 200
+    results_graph = Graph().parse(data=results.data, format="ttl")
+    assert isomorphic(results_graph, excepted_graph)
+
+
+def test_get_model_graph_not_found(client):
+    # Act
+    results = client.get("/models/-1/graph")
+
+    # Assert
+    assert results.status_code == 404
+    assert results.json == {"message": "No model with id -1"}
+
+
+def test_update_model_graph(client, building_motif):
+    # Set up
+    model = Model.create(name="my_model")
+    assert isomorphic(model.graph, default_graph)
+
+    # Action
+    results = client.patch(
+        f"/models/{model.id}/graph",
+        data=graph_data,
+        headers={"Content-Type": "application/xml"},
+    )
+    model = Model.load(model.id)
+
+    # Assert
+    assert results.status_code == 200
+    results_graph = Graph().parse(data=results.data, format="ttl")
+    expected_graph = Graph().parse(data=graph_data, format="ttl")
+    assert isomorphic(results_graph, expected_graph)
+    assert isomorphic(model.graph, expected_graph)
+
+
+def test_update_model_graph_not_found(client, building_motif):
+    # Act
+    results = client.patch(
+        "/models/-1/graph", data=graph_data, headers={"Content-Type": "application/xml"}
+    )
+
+    # Assert
+    assert results.status_code == 404
+    assert results.json == {"message": "No model with id -1"}
+
+
+def test_update_model_graph_no_header(client, building_motif):
+    # Set up
+    model = Model.create(name="my_model")
+    assert isomorphic(model.graph, default_graph)
+
+    # Action
+    results = client.patch(f"/models/{model.id}/graph", data=graph_data)
+
+    # Assert
+    assert results.status_code == 400
+
+
+def test_update_model_graph_bad_graph_value(client, building_motif):
+    # Set up
+    model = Model.create(name="my_model")
+    assert isomorphic(model.graph, default_graph)
+
+    # Action
+    results = client.patch(
+        f"/models/{model.id}/graph",
+        data="not xml",
+        headers={"Content-Type": "application/xml"},
+    )
+
+    # Assert
+    assert results.status_code == 400
