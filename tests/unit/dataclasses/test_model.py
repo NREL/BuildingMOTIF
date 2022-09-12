a3b786ec4d42f9dda7ec67101d31de152003e89c
TShapinsky <tobiasshapinsky@gmail.com>
2022-09-12 15:43:58 -0600

    commit compile bug pieces

--- tests/unit/dataclasses/test_model.py
+++ tests/unit/dataclasses/test_model.py
@@ -99,3 +99,22 @@ def test_validate_model_with_failure(bm: BuildingMOTIF):
     ctx = model.validate([shape_lib.get_shape_collection()])
     assert isinstance(ctx, ValidationContext)
     assert ctx.valid
+
+
+def test_model_compile(bm: BuildingMOTIF):
+    small_office_model = Model.create("http://example.org/building/")
+    small_office_model.graph.parse(
+        "tests/unit/fixtures/smallOffice_brick.ttl", format="ttl"
+    )
+
+    brick = Library.load(ontology_graph="libraries/brick/Brick-full.ttl")
+
+    compiled_model = small_office_model.compile([brick.get_shape_collection()])
+    precompiled_model = Graph().parse(
+        "tests/unit/fixtures/smallOffice_brick_compiled.ttl"
+    )
+    # precompiled_model = Graph().parse(data=compiled_model.serialize(format="ttl"))
+    # g = precompiled_model - compiled_model
+    # g.serialize(format="ttl", destination="model_difference.ttl")
+
+    assert isomorphic(compiled_model, precompiled_model)
