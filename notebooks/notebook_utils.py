de0d8b3654b3ce8da4b0400cee64d67f20227986
TShapinsky <tobiasshapinsky@gmail.com>
2022-10-13 15:40:43 -0600

    add docstring

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -4,6 +4,11 @@ from IPython.core.error import StdinNotImplementedError
 
 
 def notebook_input(prompt, default=None):
+    """
+    Input function which allows use of input in notebooks which are run by nbmake.
+    :param prompt: input prompt
+    :param default: default value if run by nbmake (uuid4 string if not specified)
+    """
     try:
         return input(prompt)
     except StdinNotImplementedError:
