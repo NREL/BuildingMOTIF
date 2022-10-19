1a5a88e64e94b4fd552e7f6fadc60badb784aa21
Matt Steen <matthew.steen@nrel.gov>
2022-10-19 13:19:31 -0600

    which => that per NREL Style Guide 🤓

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -5,7 +5,7 @@ from IPython.core.error import StdinNotImplementedError
 
 def notebook_input(prompt, default=None):
     """
-    Input function which allows use of input in notebooks which are run by nbmake.
+    Input function that allows use of input in notebooks, which are run by nbmake.
     :param prompt: input prompt
     :param default: default value if run by nbmake (uuid4 string if not specified)
     """
