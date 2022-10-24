12e45dc9bca46d5e38010f9e366919adf73155fd
Gabe Fierro <gtfierro@mines.edu>
2022-10-24 15:21:56 -0600

    better population, consistent URIs

--- buildingmotif/ingress.py
+++ buildingmotif/ingress.py
@@ -7,6 +7,12 @@ from buildingmotif.dataclasses import Library, Model
 from buildingmotif.ingresses.bacnet import BACnetNetwork
 
 
+def clean_uri(n) -> str:
+    if isinstance(n, str):
+        return n.replace(" ", "_")
+    return str(n)
+
+
 class IngressHandler:
     def __init__(self, bm: BuildingMOTIF):
         self.bm = bm
@@ -27,20 +33,28 @@ class BrickBACnetIngressHandler(IngressHandler):
         Adds metadata from the BACnet network into the given model
         """
         for dev in self.net.devices:
+            device_id = dev.properties.device_id
+            print(dev.properties)
+            name = clean_uri(device_id) or clean_uri(dev.properties.address)
             dev_graph = self.device_template.evaluate(
-                {"name": ns[dev.device_id], "instance-number": Literal(dev.device_id)}
+                {
+                    "name": ns[name],
+                    "instance-number": Literal(device_id),
+                    "address": Literal(dev.properties.address),
+                }
             )
             assert isinstance(dev_graph, Graph)
             m.add_graph(dev_graph)
 
-        for ((_dev_addr, dev_id), point) in self.net.objects.items():
-            obj_graph = self.object_template.evaluate(
-                {
-                    "name": ns[f"{point['name']}-{point['id']}"],
-                    "identifier": Literal(f"{point['type']},{point['id']}"),
-                    "obj-name": Literal(point["name"]),
-                    "device": ns[dev_id],
-                }
-            )
-            assert isinstance(obj_graph, Graph)
-            m.add_graph(obj_graph)
+        for ((_dev_addr, dev_id), points) in self.net.objects.items():
+            for point in points:
+                obj_graph = self.object_template.evaluate(
+                    {
+                        "name": ns[f"{clean_uri(point['name'])}-{point['address']}"],
+                        "identifier": Literal(f"{point['type']},{point['address']}"),
+                        "obj-name": Literal(point["name"]),
+                        "device": ns[clean_uri(dev_id)],
+                    }
+                )
+                assert isinstance(obj_graph, Graph)
+                m.add_graph(obj_graph)
