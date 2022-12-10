96b440833ba5f36d14f7aa45f2f2205614f2d153
Gabe Fierro <gtfierro@mines.edu>
2022-12-09 18:00:53 -0700

    add tutorial notebook and fixup ingresses

--- buildingmotif/ingresses/__init__.py
+++ buildingmotif/ingresses/__init__.py
@@ -1,9 +1,8 @@
 from buildingmotif.ingresses.bacnet import BACnetNetwork  # noqa
 from buildingmotif.ingresses.base import Record  # noqa
 from buildingmotif.ingresses.brick import BACnetToBrickIngress  # noqa
-from buildingmotif.ingresses.csv import (  # noqa
-    CSVIngress,
+from buildingmotif.ingresses.csv import CSVIngress, XLSXIngress  # noqa
+from buildingmotif.ingresses.templates import (  # noqa
     TemplateIngress,
     TemplateIngressWithChooser,
-    XLSXIngress,
 )
