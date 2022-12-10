ca383a0ca48be38e312b7e6b296bd0d719eaea2b
Gabe Fierro <gtfierro@mines.edu>
2022-12-09 18:11:27 -0700

    add another example

--- buildingmotif/ingresses/__init__.py
+++ buildingmotif/ingresses/__init__.py
@@ -2,7 +2,7 @@ from buildingmotif.ingresses.bacnet import BACnetNetwork  # noqa
 from buildingmotif.ingresses.base import Record  # noqa
 from buildingmotif.ingresses.brick import BACnetToBrickIngress  # noqa
 from buildingmotif.ingresses.csv import CSVIngress, XLSXIngress  # noqa
-from buildingmotif.ingresses.templates import (  # noqa
+from buildingmotif.ingresses.template import (  # noqa
     TemplateIngress,
     TemplateIngressWithChooser,
 )
