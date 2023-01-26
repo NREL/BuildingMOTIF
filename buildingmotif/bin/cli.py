a73f2b4c29f3e145934304caffb0a3683e42ab75
Gabe Fierro <gtfierro@mines.edu>
2023-01-25 17:57:06 -0700

    add example file, some docs

--- buildingmotif/bin/cli.py
+++ buildingmotif/bin/cli.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 import tempfile
 from pathlib import Path
 from typing import Any, Dict, Optional
@@ -53,6 +54,11 @@ def load_libraries(
     db_uri: str = typer.Argument("sqlite:///bmotif.db", envvar="DB_URI"),
     library_manifest_file: str = "libraries.yml",
 ):
+    """
+    Loads libraries from the provided YML file into the BuildingMOTIF instance at $DB_URI
+    or whatever is in 'configs.py'. Use 'get_default_libraries_yml' for the format of
+    the expected libraries.yml file
+    """
     manifest_path = Path(library_manifest_file)
     log.info(f"Loading buildingmotif libraries listed in {manifest_path}")
     bm = BuildingMOTIF(db_uri)
@@ -64,11 +70,27 @@ def load_libraries(
 
 
 @app.command()
+def get_default_libraries_yml():
+    """
+    Creates a default 'libraries.default.yml' file in the current directory
+    that can be edited and used with buildingmotif
+    """
+    default_file = (
+        Path(__file__).resolve().parents[1] / "resources" / "libraries.default.yml"
+    )
+    shutil.copyfile(default_file, "libraries.default.yml")
+    print("libraries.default.yml created in the current directory")
+
+
+@app.command()
 def serve(
     host: str = "localhost",
     port: int = 5000,
     db_uri: Optional[str] = typer.Argument(None, envvar="DB_URI"),
 ):
+    """
+    Serves the BuildingMOTIF API on the indicated host:port
+    """
     from buildingmotif.api.app import create_app
 
     if db_uri is None:
