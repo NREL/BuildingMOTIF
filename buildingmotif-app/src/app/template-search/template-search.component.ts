83b9c28e5bef0a36ed91ed2dacaf1666ce986726
Hannah Eslinger <heslinge@nrel.gov>
2023-02-08 16:28:21 -0700

    Remove template search resolver

--- buildingmotif-app/src/app/template-search/template-search.component.ts
+++ buildingmotif-app/src/app/template-search/template-search.component.ts
@@ -3,7 +3,6 @@ import { TemplateSearchService, Template } from './template-search.service';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-template-search',
@@ -12,24 +11,32 @@ import { ActivatedRoute } from '@angular/router';
   providers: [TemplateSearchService],
 })
 export class TemplateSearchComponent implements OnInit{
-  templates: Template[] = [];
+  error: any = undefined;
+  templates: Template[] | undefined = undefined;
   fitlerStringControl = new FormControl('');
   filteredTemplates: Observable<Template[]> = new Observable();
 
-  constructor(private route: ActivatedRoute) {
-    this.templates = this.route.snapshot.data["templateSearch"];
-  }
+  constructor(private TemplateSearchService: TemplateSearchService){}
 
-  private _filterTemplatesByName(value: string): Template[] {
-    const filterValue = value.toLowerCase();
-
-    return this.templates.filter(template => template.name.toLocaleLowerCase().includes(filterValue))
-  }
-
-  ngOnInit() {
+  private _setTemplates(data: Template[]): void {
+    this.templates = data;
     this.filteredTemplates = this.fitlerStringControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filterTemplatesByName(value || '')),
     );
+  }
+
+  private _filterTemplatesByName(value: string): Template[] {
+    const filterValue = value.toLowerCase();
+    if(this.templates == undefined) return [];
+    return this.templates.filter(template => template.name.toLocaleLowerCase().includes(filterValue))
+  }
+
+  ngOnInit() {
+    this.TemplateSearchService.getAllTemplates()
+      .subscribe({
+        next: (data: Template[]) => this._setTemplates(data), // success path
+        error: (error) => this.error = error // error path
+      });
   }
 }
