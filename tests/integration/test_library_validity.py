b920f0e326b3ad6b7961ed05fe22aa68c22cfbc4
Gabe Fierro <gtfierro@mines.edu>
2023-02-25 13:06:47 -0700

    update tests, fix templates

--- tests/integration/test_library_validity.py
+++ tests/integration/test_library_validity.py
@@ -1,9 +1,18 @@
 from pathlib import Path
+from typing import Set
 
 import pytest
 from rdflib import Graph, Namespace
 
 from buildingmotif.dataclasses import Library, Model
+
+# these are templates that are difficult to test individually
+# but are covered indirectly by other tests
+S223_SKIP_TEMPLATES: Set[str] = {
+    "duct",
+    "sensor",
+    "differential-sensor",
+}
 
 
 @pytest.mark.integration
@@ -15,6 +24,9 @@ def test_223p_library(bm, library_path_223p: Path):
     MODEL = Namespace("urn:ex/")
     for templ in lib.get_templates():
         print(templ.name)
+        if templ.name in S223_SKIP_TEMPLATES:
+            print(" ...skipping")
+            continue
         m = Model.create(MODEL)
         _, g = templ.inline_dependencies().fill(MODEL)
         assert isinstance(g, Graph), "was not a graph"
