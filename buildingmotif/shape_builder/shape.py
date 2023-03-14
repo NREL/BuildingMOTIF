ad4a00bb0bd1845d70ef1ec8146890e513396002
TShapinsky <tobiasshapinsky@gmail.com>
2023-03-14 16:40:25 -0400

    add docstrings to shape methods

--- buildingmotif/shape_builder/shape.py
+++ buildingmotif/shape_builder/shape.py
@@ -8,11 +8,20 @@ from buildingmotif.namespaces import SH, A, bind_prefixes
 
 
 class Shape(Graph):
+    """Base class for constructing shapes programatically"""
+
     def __init__(
         self,
         identifier: Optional[Union[Node, str]] = None,
         message: Optional[str] = None,
     ) -> None:
+        """
+
+        :param identifier: id for shape
+        :type identifier: Optional[Union[Node, str]]
+        :param message: sh:message annotation
+        :type message: str
+        """
         super().__init__(identifier=identifier)
         bind_prefixes(self)
 
@@ -20,6 +29,15 @@ class Shape(Graph):
             self.add((self.identifier, SH["message"], Literal(message)))
 
     def add_property(self, property: URIRef, object: Node):
+        """Add property to shape
+
+        :param property: ref of property
+        :type property: URIRef
+        :param object: ref of object
+        :type object: Node"""
+        # This same functionality could be added with just use of add.
+        # This design is useful to allow adding properties in the
+        # builder paradigm.
         self.add((self, property, object))
 
         return self
@@ -27,6 +45,12 @@ class Shape(Graph):
     def add_list_property(
         self, property: URIRef, nodes: Union[List[Node], Tuple[Node, ...]]
     ):
+        """Add property which references list to shape
+
+        :param property: ref of property
+        :type property: URIRef
+        :param nodes: nodes to include in list
+        :type nodes: Union[List[Node], Tuple[Node, ...]]"""
         identifier = BNode()
         Collection(self, identifier, nodes)
 
@@ -35,25 +59,47 @@ class Shape(Graph):
         return self
 
     def OR(self, *nodes: Node):
+        """add OR property
+
+        :param nodes: list of nodes to OR
+        :type nodes: Union[List[Node], Tuple[Node, ...]]
+        """
         self.add_list_property(SH["or"], nodes)
         return self
 
     def AND(self, *nodes: Node):
+        """add AND property
+
+        :param nodes: list of nodes to AND
+        :type nodes: Union[List[Node], Tuple[Node, ...]]
+        """
         self.add_list_property(SH["and"], nodes)
         return self
 
     def NOT(self, node: Node):
+        """add NOT property
+
+        :param nodes: list of nodes to NOT
+        :type nodes: Union[List[Node], Tuple[Node, ...]]
+        """
         self.add((self, SH["not"], node))
 
         return self
 
     def XONE(self, *nodes: Node):
+        """add XONE property
+
+        :param nodes: list of nodes to XONE
+        :type nodes: Union[List[Node], Tuple[Node, ...]]
+        """
         self.add_list_property(SH["xone"], nodes)
         return self
 
     def add(self, triple: Tuple[Node, Node, Node]):
         (s, p, o) = triple
         if isinstance(o, Shape):
+            # if the object being added is of type Shape
+            # add the whole graph to this graph
             self += o
             o = o.identifier
         if s == self:
@@ -63,23 +109,42 @@ class Shape(Graph):
 
 
 class NodeShape(Shape):
+    """Class for constructing Node Shapes programatically"""
+
     def __init__(
         self,
         identifier: Optional[Union[Node, str]] = None,
         message: Optional[str] = None,
     ) -> None:
+        """
+
+        :param identifier: id for shape
+        :type identifier: Optional[Union[Node, str]]
+        :param message: sh:message annotation
+        :type message: str
+        """
         super().__init__(identifier=identifier, message=message)
 
         self.add((self, A, SH["NodeShape"]))
 
-    def of_class(self, class_: Node, target=False):
-        predicate = predicate = SH["target_class"] if target else SH["class"]
+    def of_class(self, class_: Node, active=False):
+        """Add constraint that target much be of a certain class
+
+        :param class_: class of target
+        :type class_: Node
+        :param active: should shape actively target the class or not
+        :type active: bool"""
+        predicate = predicate = SH["target_class"] if active else SH["class"]
 
         self.add((self, predicate, class_))
 
         return self
 
-    def has_property(self, property: Node):
+    def has_property(self, property: Union[Node, URIRef]):
+        """Add property constraint.
+
+        :param property: property shape or property to add constraint for
+        :type property: Union[Node, URIRef]"""
         if isinstance(property, URIRef):
             property = PropertyShape().has_path(property)
 
@@ -94,6 +159,13 @@ class PropertyShape(Shape):
         identifier: Optional[Union[Node, str]] = None,
         message: Optional[str] = None,
     ) -> None:
+        """
+
+        :param identifier: id for shape
+        :type identifier: Optional[Union[Node, str]]
+        :param message: sh:message annotation
+        :type message: str
+        """
         super().__init__(identifier=identifier, message=message)
 
         self.add((self, RDF.type, SH["PropertyShape"]))
@@ -105,6 +177,17 @@ class PropertyShape(Shape):
         zero_or_more: bool = False,
         one_or_more: bool = False,
     ) -> "PropertyShape":
+        """Add path constraint to shape.
+        zero_or_one, zero_or_more, and one_or_more flags are mutually exclusive
+
+        :param path: path to add constraint for
+        :type path: Node
+        :param zero_or_one: match zero or one instances of path
+        :type zero_or_one: bool
+        :param zero_or_more: match zero or more instances of path
+        :type zero_or_more: bool
+        :param one_or_more: match one or more instances of path
+        :type one_or_more: bool"""
 
         if zero_or_one or zero_or_more or one_or_more:
             path_constraint = None
@@ -127,8 +210,24 @@ class PropertyShape(Shape):
         min: int = None,
         max: int = None,
         exactly: int = None,
-        qualified=False,
+        qualified: bool = False,
     ):
+        """Add target matches constraint to property shape
+
+        :param target: target node to specify what should be matched, usually shape or class
+        :type target: Node
+        :param type: sh:class or sh:node
+        :type type: URIRef
+        :param min: min count of matched entities
+        :type min: int
+        :param max: max count of matched entities
+        :type max: int
+        :param exactly: exact count of matched entities (takes precidence over min/max)
+        :type exactly: int
+        :param qualified: Is this property qualified or universal
+        :type qualified: bool
+
+        """
         if min is None and max is None and exactly is None:
             if qualified:
                 raise ValueError("min, max or exactly must have a value")
@@ -163,6 +262,19 @@ class PropertyShape(Shape):
         exactly: int = None,
         qualified=False,
     ):
+        """Add target matches class constraint to property shape
+
+        :param class_: target class what should be matched
+        :type class_: Node
+        :param min: min count of matched entities
+        :type min: int
+        :param max: max count of matched entities
+        :type max: int
+        :param exactly: exact count of matched entities (takes precidence over min/max)
+        :type exactly: int
+        :param qualified: Is this property qualified or universal
+        :type qualified: bool
+        """
         return self.matches(class_, SH["class"], min, max, exactly, qualified)
 
     def matches_shape(
@@ -173,20 +285,53 @@ class PropertyShape(Shape):
         exactly: int = None,
         qualified=False,
     ):
+        """Add target matches shape constraint to property shape
+
+        :param shape: target shape what should be matched
+        :type shape: Node
+        :param min: min count of matched entities
+        :type min: int
+        :param max: max count of matched entities
+        :type max: int
+        :param exactly: exact count of matched entities (takes precidence over min/max)
+        :type exactly: int
+        :param qualified: Is this property qualified or universal
+        :type qualified: bool
+        """
         return self.matches(shape, SH["node"], min, max, exactly, qualified)
 
 
 def OR(*nodes: Node) -> Shape:
+    """add OR property
+
+    :param nodes: list of nodes to OR
+    :type nodes: Union[List[Node], Tuple[Node, ...]]
+    """
     return Shape().OR(*nodes)
 
 
 def AND(*nodes: Node) -> Shape:
+    """add AND property
+
+    :param nodes: list of nodes to AND
+    :type nodes: Union[List[Node], Tuple[Node, ...]]
+    """
     return Shape().AND(*nodes)
 
 
 def NOT(node: Node) -> Shape:
+    """add NOT property
+
+    :param nodes: list of nodes to NOT
+    :type nodes: Union[List[Node], Tuple[Node, ...]]
+    """
     return Shape().NOT(node)
 
 
 def XONE(*nodes: Node) -> Shape:
+    """add XONE property
+
+    :param nodes: list of nodes to XONE
+    :type nodes: Union[List[Node], Tuple[Node, ...]]
+    """
     return Shape().XONE(*nodes)
