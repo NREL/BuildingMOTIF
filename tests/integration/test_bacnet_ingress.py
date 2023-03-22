2da26f993c251f4745831655c7f285cd02abdff1
Gabe Fierro <gtfierro@mines.edu>
2023-03-22 16:27:42 -0600

    use setup/teardown for bacnet test

--- tests/integration/test_bacnet_ingress.py
+++ tests/integration/test_bacnet_ingress.py
@@ -17,23 +17,24 @@ docker_compose_start = shlex.split("docker compose up -d")
 docker_compose_stop = shlex.split("docker compose down")
 
 
+@pytest.fixture()
+def bacnet_network():
+    subprocess.run(docker_compose_start, cwd=docker_compose_path)
+    yield
+    subprocess.run(docker_compose_stop, cwd=docker_compose_path)
+
+
 @pytest.mark.integration
-def test_bacnet_ingress(bm):
-    try:
-        subprocess.run(docker_compose_start, cwd=docker_compose_path)
-        BLDG = Namespace("urn:building/")
-        m = Model.create(BLDG, "test building for bacnet scan")
-        bacnet = BACnetNetwork("172.24.0.1/32")
-        tobrick = BACnetToBrickIngress(bm, bacnet)
-        m.add_graph(tobrick.graph(BLDG))
+def test_bacnet_ingress(bm, bacnet_network):
+    BLDG = Namespace("urn:building/")
+    m = Model.create(BLDG, "test building for bacnet scan")
+    bacnet = BACnetNetwork("172.24.0.1/32")
+    tobrick = BACnetToBrickIngress(bm, bacnet)
+    m.add_graph(tobrick.graph(BLDG))
 
-        devices = list(m.graph.subjects(RDF["type"], BACNET["BACnetDevice"]))
-        assert (
-            len(devices) == 1
-        ), f"Did not find exactly 1 device (found {len(devices)})"
-        assert devices[0] == BLDG["599"]  # type: ignore
+    devices = list(m.graph.subjects(RDF["type"], BACNET["BACnetDevice"]))
+    assert len(devices) == 1, f"Did not find exactly 1 device (found {len(devices)})"
+    assert devices[0] == BLDG["599"]  # type: ignore
 
-        objects = list(m.graph.subjects(RDF["type"], BRICK["Point"]))
-        assert len(objects) == 2, "Did not find exactly 2 points"
-    finally:
-        subprocess.run(docker_compose_stop, cwd=docker_compose_path)
+    objects = list(m.graph.subjects(RDF["type"], BRICK["Point"]))
+    assert len(objects) == 2, "Did not find exactly 2 points"
