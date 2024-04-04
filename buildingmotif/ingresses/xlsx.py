fbece36aa18fbb511ba8b2643bd16c7f9c4aec8a
Gabe Fierro <gtfierro225@gmail.com>
2024-04-04 14:46:54 -0600

    put limit on xlsx

--- buildingmotif/ingresses/xlsx.py
+++ buildingmotif/ingresses/xlsx.py
@@ -1,7 +1,7 @@
 import logging
 from functools import cached_property
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from buildingmotif.ingresses.base import Record, RecordIngressHandler
 
@@ -18,7 +18,7 @@ class XLSXIngress(RecordIngressHandler):
     field of each Record gives the name of the sheet.
     """
 
-    def __init__(self, filename: Path):
+    def __init__(self, filename: Path, limit: Optional[int] = -1):
         """
         Path to the .xlsx file to be ingested
 
@@ -27,6 +27,7 @@ class XLSXIngress(RecordIngressHandler):
         """
 
         self.filename = filename
+        self.limit = limit or -1
 
     @cached_property
     def records(self) -> List[Record]:
@@ -39,11 +40,12 @@ class XLSXIngress(RecordIngressHandler):
         :rtype: List[Record]
         """
         records = []
-        wb = load_workbook(self.filename)  # noqa
+        wb = load_workbook(self.filename, data_only=True)  # noqa
         for sheetname in wb.sheetnames:
             sheet = wb[sheetname]
             columns = [sheet.cell(1, c + 1).value for c in range(sheet.max_column)]
-            for row in range(2, sheet.max_row + 1):
+            upper_range = sheet.max_row + 1 if self.limit < 0 else self.limit
+            for row in range(2, upper_range):
                 fields = {
                     columns[c]: sheet.cell(row, c + 1).value
                     for c in range(sheet.max_column)
