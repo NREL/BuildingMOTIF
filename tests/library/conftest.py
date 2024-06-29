84f5fde596270c2bdbd2ca07d214d6f48a3fb252
Gabe Fierro <gtfierro@mines.edu>
2024-06-29 09:35:12 -0600

    add comments for explaining the library conftest

--- tests/library/conftest.py
+++ tests/library/conftest.py
@@ -21,6 +21,16 @@ from buildingmotif.database.utils import (
 )
 from buildingmotif.namespaces import bind_prefixes
 
+# The fact that BuildingMOTIF is a singleton class is a problem for testing templates.
+# We want to have tests which are parameterized by the library and template name; this makes
+# it possible to use pytest filters (with the "-k" flag) to run tests for specific libraries or templates.
+# We want each (library, template) pair to operate in a "clean" environment, so that the tests are isolated.
+# However, the singleton pattern means that the BuildingMOTIF instance is shared across all tests.
+# We can work around this by patching BuildingMOTIF to ignore the singleton pattern.
+
+# "instances" is a dictionary that maps the name of the module to the BuildingMOTIF instance.
+instances = {}
+
 
 # non-singleton BuildingMOTIF
 class BuildingMOTIF:
@@ -132,17 +142,6 @@ class BuildingMOTIF:
         self.engine.dispose()
 
 
-# The fact that BuildingMOTIF is a singleton class is a problem for testing templates.
-# We want to have tests which are parameterized by the library and template name; this makes
-# it possible to use pytest filters (with the "-k" flag) to run tests for specific libraries or templates.
-# We want each (library, template) pair to operate in a "clean" environment, so that the tests are isolated.
-# However, the singleton pattern means that the BuildingMOTIF instance is shared across all tests.
-# We can work around this by patching BuildingMOTIF to ignore the singleton pattern.
-
-# "instances" is a dictionary that maps the name of the module to the BuildingMOTIF instance.
-instances = {}
-
-
 # The normal get_building_motif method uses the fact that BuildingMOTIF is a singleton class.
 # We need to mock this method so that we can create a new instance of BuildingMOTIF for each test.
 # We use the "instances" dictionary to store the BuildingMOTIF instance for each module.
@@ -159,9 +158,10 @@ def mock_building_motif():
     return instances[name]
 
 
+# This is a context ("with PatchBuildingMotif()") that patches the "get_building_motif" method
+# to use the correct BuildingMOTIF instance for each test. We have to patch the get_building_motif
 class PatchBuildingMotif:
     def __enter__(self):
-        print("patching")
         self.monkeypatch = MonkeyPatch()
         self.monkeypatch.setattr(
             buildingmotif.dataclasses.library, "get_building_motif", mock_building_motif
@@ -190,5 +190,4 @@ class PatchBuildingMotif:
         return self.monkeypatch
 
     def __exit__(self, *args):
-        print("undoing patch")
         self.monkeypatch.undo()
