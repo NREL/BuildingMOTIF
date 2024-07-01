cb9f08f1b880f28bc74c2f83e5605908d1ec094a
Gabe Fierro <gtfierro@mines.edu>
2024-07-01 09:52:41 -0600

    add library tests for github, add back notebook fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,9 @@
 """
 Generates tests automatically
 """
+import glob
+from pathlib import Path
+
 import pytest
 
 from buildingmotif import BuildingMOTIF
@@ -19,3 +22,18 @@ def bm():
     bm.close()
     # clean up the singleton so that tables are re-created correctly later
     BuildingMOTIF.clean()
+
+
+def pytest_generate_tests(metafunc):
+    """
+    Generates BuildingMOTIF tests for a variety of contexts
+    """
+
+    # validates that example files pass validation
+    if "notebook" in metafunc.fixturenames:
+        notebook_files = [
+            Path(notebook)
+            for notebook in glob.glob("notebooks/**/*.ipynb", recursive=True)
+        ]
+
+        metafunc.parametrize("notebook", notebook_files)
