46c997e6e049ebc490b737c4f774ad952c749a5a
Gabe Fierro <gtfierro@mines.edu>
2024-07-03 23:44:27 -0600

    remove unneeded flag

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--skip-library-tests",
-        action="store_true",
-        help="Skip pytest_generate_tests hook for library validity tests",
-    )
