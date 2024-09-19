8e99932f255d51ec930a246f32e2e855c308d349
Gabe Fierro <gtfierro@mines.edu>
2024-09-18 20:43:04 -0600

    use the correct SHACL engine

--- buildingmotif/dataclasses/model.py
+++ buildingmotif/dataclasses/model.py
@@ -274,7 +274,7 @@ class Model:
                 temp_model_graph.add((URIRef(s), A, shape_uri))
 
             valid, report_g, report_str = shacl_validate(
-                temp_model_graph, ontology_graph
+                temp_model_graph, ontology_graph, engine=self._bm.shacl_engine
             )
 
             results[shape_uri] = ValidationContext(
