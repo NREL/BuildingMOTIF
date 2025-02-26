6d7386565ed0ad7800ae827236f695154d467892
TShapinsky <tobiasshapinsky@gmail.com>
2025-02-26 09:53:57 -0700

    patch test by using overwrite=False to prevent library deletion cascade

--- tests/unit/api/test_model.py
+++ tests/unit/api/test_model.py
@@ -262,7 +262,9 @@ def test_validate_model(client, building_motif, shacl_engine):
     assert library_1 is not None
     library_2 = Library.load(directory="tests/unit/fixtures/templates")
     assert library_2 is not None
-    brick = Library.load(ontology_graph="tests/unit/fixtures/Brick.ttl")
+    brick = Library.load(
+        ontology_graph="tests/unit/fixtures/Brick.ttl", overwrite=False
+    )
     assert brick is not None
 
     BLDG = Namespace("urn:building/")
